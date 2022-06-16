303908ef20a9a811584a62e8bbd5534616949094
guo32 <guo32@users.noreply.github.com>
2022-06-16 15:21:33 +0900

    First commit in dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,7 +3,9 @@ public class MyClass {
 
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
-		System.out.println("version 2");
+		System.out.println("This is ??? version.");
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 	}
 	
 }
